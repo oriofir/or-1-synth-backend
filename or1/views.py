8bd6f4aa89bd9ab8f702418ccc9ebec38a28a7ef
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-13 12:20:28 -0700

    views created

--- or1/views.py
+++ or1/views.py
@@ -1,3 +1,17 @@
+from rest_framework import generics
 from django.shortcuts import render
 
+from .serializers import SynthSerializer
+from .models import Synth
+
 # Create your views here.
+
+
+class SynthList(generics.ListCreateAPIView):
+    queryset = Synth.objects.all()
+    serializer_class = SynthSerializer
+
+
+class SynthDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Synth.objects.all()
+    serializer_class = SynthSerializer
