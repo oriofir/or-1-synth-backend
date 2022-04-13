70be29bc851e83cdec6c4a809bfe2c7cd1326342
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-13 00:28:12 -0700

    started making synth model

--- or1/models.py
+++ or1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Synth(models.Model):
+    name: models.CharField(max_length=200)
+    synth_type: models.CharField(max_length=200)
+    filter_amount: models.IntegerField(blank=True, null=True)
+    delay_amount: models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
