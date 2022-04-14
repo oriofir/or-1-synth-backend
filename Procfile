0434bed198ca576d6de3fbe7930321dc5863aa05
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-13 21:45:46 -0700

    added static files and heroku build setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn or1_django.wsgi
--- or1_django/settings.py
+++ or1_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 CORS_ALLOW_ALL_ORIGINS = True
 
